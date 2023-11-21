ef05a09e1aadd23598d249c7509bcd8bb5fd7fc9
doyoon <doyoun0527@naver.com>
2023-11-21 15:48:31 +0900

    init commit

---
+++ src/main/java/com/example/teampproject/TeamPProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.teampproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamPProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TeamPProjectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/teampproject/TeamPProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teampproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamPProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
